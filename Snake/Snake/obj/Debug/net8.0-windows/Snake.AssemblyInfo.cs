866936060499777c485a2dbe94ccd5460b32a109
nadiina <nadiia.shcher@gmail.com>
2024-02-10 16:20:27 +0200

    add

--- Snake/Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c65cdac8c2cc6d36a2f3376419930fe4f146f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6553381ab056604f89cfe74caeda847ed5de1dcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
